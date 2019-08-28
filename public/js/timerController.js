9697e450588a4d82a95503f4e597b7be49bcde34
Mark Toogood <mark@toogood.id.au>
2019-08-28 21:03:08 +1000

    changing timings and var names

--- public/js/timerController.js
+++ public/js/timerController.js
@@ -35,19 +35,6 @@ var resetTimer = function() {
   sendResetSignal();
 }
 
-function DoW(days) {
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-
-  return weekday[days];
-}
-
 var updateDisplay = function(days, hours, minutes, seconds) {
   if (displayReady) {
     dowdisplay.innerText = DoW(days);
